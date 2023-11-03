958e6e3eab60d4511b487fdc0e74ce01eaf70d18
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-11-03 07:55:17 +0000

    02counterproject

---
+++ 02counterproject/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    let [counter,setCounter]=useState(0)
+    function increase(){
+      if(counter>19)
+        return
+        setCounter(counter+1)
+    }
+    function decrease(){
+      if(counter<1)
+      return
+      setCounter(counter-1)
+    }
+
+  return (
+    <>
+<h1>AGE:{counter}</h1>
+  <button onClick={increase}>INCREASE :{counter}</button>
+  <br />
+  <button onClick={decrease}>REDUCE:{counter}</button>
+    </>
+  )
+}
+
+export default App
