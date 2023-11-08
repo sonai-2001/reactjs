12fb35762527b4e1a0158a13f5dadc9f711152d9
sonai-2001 <147368308+sonai-2001@users.noreply.github.com>
2023-11-08 07:25:34 +0000

    react rendering

---
+++ Reactapp/demo.js
@@ -0,0 +1,6 @@
+
+const element=React.createElement('div',{id:'div1'},
+React.createElement('h1',null,'Apurba'),
+React.createElement('input',{type:'text'},null))
+
+ReactDOM.render(element,document.getElementById('container'))
